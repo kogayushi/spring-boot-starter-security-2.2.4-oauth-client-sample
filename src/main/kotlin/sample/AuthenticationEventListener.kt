630d6b893d2965d20faab6173583355351f65d17
yushi.koga <youshe.old.pleasure@gmail.com>
2020-02-25 23:08:32 +0900

    listen InteractiveAuthenticationSuccessEvent
    then output debug log for development

---
+++ src/main/kotlin/sample/AuthenticationEventListener.kt
@@ -0,0 +1,22 @@
+package sample
+
+import org.slf4j.LoggerFactory
+import org.springframework.context.event.EventListener
+import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent
+import org.springframework.stereotype.Component
+
+@Component
+class AuthenticationEventListener {
+    val logger = LoggerFactory.getLogger(AuthenticationEventListener::class.java)
+
+    // InteractiveAuthenticationSuccessEventをlistenするのが周りくどいのであれば、
+    // OidcUserServiceやOAuth2UserServiceを拡張したクラスで永続化するやり方もある
+    // その場合（私は）なぜか、@Transactionalを忘れがちなので、忘れずにつけること。
+    // @Tranctional
+    @EventListener
+    fun listen(event: InteractiveAuthenticationSuccessEvent) {
+        event.authentication.principal
+        logger.debug("event {}", event)
+        // TODO ユーザ情報の永続化
+    }
+}
