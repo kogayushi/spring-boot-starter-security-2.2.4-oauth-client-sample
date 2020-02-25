2d58855c2056002404bf9618c43fa0e1522c625d
yushi.koga <youshe.old.pleasure@gmail.com>
2020-02-26 08:42:24 +0900

    save user principal

--- src/main/kotlin/sample/eventlistener/AuthenticationEventListener.kt
+++ src/main/kotlin/sample/eventlistener/AuthenticationEventListener.kt
@@ -4,19 +4,22 @@ import org.slf4j.LoggerFactory
 import org.springframework.context.event.EventListener
 import org.springframework.security.authentication.event.InteractiveAuthenticationSuccessEvent
 import org.springframework.stereotype.Component
+import sample.oidc.CustomOAuth2User
+import sample.user.InMemoryMyUserPrincipalRepository
 
 @Component
-class AuthenticationEventListener {
-    val logger = LoggerFactory.getLogger(AuthenticationEventListener::class.java)
-
+class AuthenticationEventListener(private val repository: InMemoryMyUserPrincipalRepository) {
+    companion object {
+        val logger = LoggerFactory.getLogger(AuthenticationEventListener::class.java)
+    }
     // InteractiveAuthenticationSuccessEventをlistenするのが周りくどいのであれば、
     // OidcUserServiceやOAuth2UserServiceを拡張したクラスで永続化するやり方もある
     // その場合（私は）なぜか、@Transactionalを忘れがちなので、忘れずにつけること。
     // @Tranctional
     @EventListener
     fun listen(event: InteractiveAuthenticationSuccessEvent) {
-        event.authentication.principal
         logger.debug("event {}", event)
-        // TODO ユーザ情報の永続化
+        val user = event.authentication.principal as CustomOAuth2User
+        repository.save(user.toMyUserPrincipal())
     }
 }
